71ec857c696cf809865cab221f3f74daa9401615
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-07-27 21:54:55 -0500

    starting to update pies

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
 const pies = [
   {
-    name: "Dutch Apple Pie",
-    ingredients: "apples,sugar,butter,nutmeg,dutch people",
+    name: 'Dutch Apple Pie',
+    ingredients: 'apples,sugar,butter,nutmeg,dutch people',
     bakeTemp: 5000,
-    drinkPairing: "Earl Grey Tea",
+    drinkPairing: 'Earl Grey Tea',
     imageUrl:
-      "https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg",
-    instructor: "Doc",
-    iceCream: "Vanilla"
+      'https://images-gmi-pmc.edge-generalmills.com/b6a2a4e7-73f5-4aec-9bb6-f2b5054d65e6.jpg',
+    instructor: 'Doc',
+    iceCream: 'Vanilla',
   },
   {
-    name: "Berry Pie",
-    ingredients: "berries",
+    name: 'Berry Pie',
+    ingredients: 'berries',
     bakeTemp: 400,
-    drinkPairing: "wine",
+    drinkPairing: 'wine',
     imageUrl:
-      "https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg",
-    instructor: "Doc",
-    iceCream: "banana"
+      'https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg',
+    instructor: 'Doc',
+    iceCream: 'banana',
   },
   {
-    name: "Pumpkin Pie",
-    ingredients: "pumpkins, nutmeg, cinnamon, graham crackers, pilgrims",
+    name: 'Pumpkin Pie',
+    ingredients: 'pumpkins, nutmeg, cinnamon, graham crackers, pilgrims',
     bakeTemp: 42,
-    drinkPairing: "egg nog",
+    drinkPairing: 'egg nog',
     imageUrl:
-      "https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg",
-    instructor: "Aja",
-    iceCream: "Vanilla"
+      'https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg',
+    instructor: 'Aja',
+    iceCream: 'Vanilla',
   },
   {
-    name: "Shoo Fly Pie",
-    ingredients: "Molasses, nutmeg, cinnamon, butter, graham cracker ",
+    name: 'Shoo Fly Pie',
+    ingredients: 'Molasses, nutmeg, cinnamon, butter, graham cracker ',
     bakeTemp: 1234,
-    drinkPairing: "Apple Cider",
+    drinkPairing: 'Apple Cider',
     imageUrl:
-      "https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg",
-    instructor: "Aja",
-    iceCream: "Coffee"
+      'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+    instructor: 'Aja',
+    iceCream: 'Coffee',
   },
   {
-    name: "Pecan Pie",
-    ingredients: "Pecans, sugar, butter, flour",
+    name: 'Pecan Pie',
+    ingredients: 'Pecans, sugar, butter, flour',
     bakeTemp: 5000,
-    drinkPairing: "Milk",
+    drinkPairing: 'Milk',
     imageUrl:
-      "https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg",
-    instructor: "Trinity",
-    iceCream: "Vanilla"
+      'https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg',
+    instructor: 'Trinity',
+    iceCream: 'Vanilla',
   },
   {
-    name: "Keylime Pie",
-    ingredients: "lemons, sugar, butter, flour",
+    name: 'Keylime Pie',
+    ingredients: 'lemons, sugar, butter, flour',
     bakeTemp: 5000,
-    drinkPairing: "Water",
+    drinkPairing: 'Water',
     imageUrl:
-      "https://www.browneyedbaker.com/wp-content/uploads/2012/05/key-lime-pie-2-1200.jpg",
-    instructor: "Trinity",
-    iceCream: "none"
-  }
+      'https://www.browneyedbaker.com/wp-content/uploads/2012/05/key-lime-pie-2-1200.jpg',
+    instructor: 'Trinity',
+    iceCream: 'none',
+  },
 ];
 
 const renderToDom = (divId, textToPrint) => {
@@ -74,46 +74,46 @@ const buttons = () => {
   <button type="button" class="btn btn-danger" id="Doc">Doc</button>
   `;
 
-  renderToDom("#buttonContainer", domString);
+  renderToDom('#buttonContainer', domString);
 };
 
 // display form on the DOM
-const pieForm = () => {
+const pieForm = (pieObject = {}) => {
   const domString = `
     <form id="pieFormForm">
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input required type="text" class="form-control" id="name">
+        <input required type="text" value="${pieObject.name || ""}" class="form-control" id="name">
       </div>
       <div class="mb-3">
         <label for="ingredients" class="form-label">Ingredients</label>
-        <input required type="text" class="form-control" id="ingredients">
+        <input required type="text" value="${pieObject.ingredients || ""}" class="form-control" id="ingredients">
       </div>
       <div class="mb-3">
       <label for="bakeTemp" class="form-label">Bake Temp</label>
-      <input required type="number" class="form-control" id="bakeTemp">
+      <input required type="number" value="${pieObject.bakeTemp || ""}" class="form-control" id="bakeTemp">
       </div>
       <div class="mb-3">
         <label for="drinkPairing" class="form-label">Drink Pairing</label>
-        <input required type="text" class="form-control" id="drinkPairing">
+        <input required type="text" class="form-control" value="${pieObject.drinkPairing || ""}" id="drinkPairing">
       </div>
       <div class="mb-3">
         <label for="imageUrl" class="form-label">Image URL</label>
-        <input required type="url" class="form-control" id="imageUrl">
+        <input required type="url" class="form-control" value="${pieObject.imageUrl || ""}" id="imageUrl">
       </div>
       <div class="mb-3">
         <label for="instructor" class="form-label">Instructor</label>
-        <input required type="text" class="form-control" id="instructor">
+        <input required type="text" class="form-control" value="${pieObject.instructor || ""}" id="instructor">
       </div>
       <div class="mb-3">
         <label for="iceCream" class="form-label">Ice Cream</label>
-        <input required type="text" class="form-control" id="iceCream">
+        <input required type="text" class="form-control" value="${pieObject.iceCream || ""}" id="iceCream">
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   `;
 
-  renderToDom("#pieForm", domString);
+  renderToDom('#pieForm', domString);
 };
 
 const handleFormSubmit = (event) => {
@@ -121,13 +121,13 @@ const handleFormSubmit = (event) => {
 
   // TODO: Update the Id's in thie object
   const newPie = {
-    name: document.querySelector("#name").value,
-    ingredients: document.querySelector("#ingredients").value,
-    bakeTemp: document.querySelector("#bakeTemp").value,
-    drinkPairing: document.querySelector("#drinkPairing").value,
-    imageUrl: document.querySelector("#imageUrl").value,
-    instructor: document.querySelector("#instructor").value,
-    iceCream: document.querySelector("#iceCream").value
+    name: document.querySelector('#name').value,
+    ingredients: document.querySelector('#ingredients').value,
+    bakeTemp: document.querySelector('#bakeTemp').value,
+    drinkPairing: document.querySelector('#drinkPairing').value,
+    imageUrl: document.querySelector('#imageUrl').value,
+    instructor: document.querySelector('#instructor').value,
+    iceCream: document.querySelector('#iceCream').value,
   };
   pies.push(newPie);
   pieBuilder(pies);
@@ -137,19 +137,31 @@ const handleFormSubmit = (event) => {
   console.log(pies);
 };
 
-const deletePie = (event) => {
+const modifyPie = (event) => {
   const targetId = event.target.id;
   const targetType = event.target.type;
 
-  if (targetType === "button") {
-    pies.splice(targetId, 1);
-    pieBuilder(pies);
+  if (targetType === 'button') {
+    const [method, id] = targetId.split("--");
+
+    if (method === "delete") {
+      // const id = targetId.split("--")[1]
+      // EXAMPLE DESTRUCTURING AN ARRAY
+      // const array = ["Madden", "Elle", "Halie"];
+      // const [,, p3] = array;
+      // console.log(id);
+      pies.splice(id, 1);
+      pieBuilder(pies);
+    } else {
+      const pieObject =  pies[id];
+      pieForm(pieObject);
+    }
   }
 };
 
 const pieFormEvents = () => {
-  const pieFormElement = document.querySelector("#pieFormForm");
-  pieFormElement.addEventListener("submit", handleFormSubmit);
+  const pieFormElement = document.querySelector('#pieFormForm');
+  pieFormElement.addEventListener('submit', handleFormSubmit);
 };
 
 const filterPies = (array, instructor) => {
@@ -157,25 +169,25 @@ const filterPies = (array, instructor) => {
 };
 
 const handleButtonClick = (event) => {
-  if (event.target.id === "All") {
+  if (event.target.id === 'All') {
     pieBuilder(pies);
   }
-  if (event.target.id === "Trinity") {
+  if (event.target.id === 'Trinity') {
     const trinPies = filterPies(pies, event.target.id);
     pieBuilder(trinPies);
   }
-  if (event.target.id === "Aja") {
+  if (event.target.id === 'Aja') {
     const ajasPies = filterPies(pies, event.target.id);
     pieBuilder(ajasPies);
   }
-  if (event.target.id === "Doc") {
+  if (event.target.id === 'Doc') {
     const docsPies = filterPies(pies, event.target.id);
     pieBuilder(docsPies);
   }
 };
 
 const pieBuilder = (piesArray) => {
-  let domString = "";
+  let domString = '';
   piesArray.forEach((pie, i) => {
     domString += `
     <div class="card" style="width: 18rem;">
@@ -183,22 +195,23 @@ const pieBuilder = (piesArray) => {
       <div class="card-body">
         <h5 class="card-title">${pie.name}</h5>
         <p class="card-text">${pie.ingredients}</p>
-        <button type="button" id=${i} class="btn btn-primary">Delete</button>
+        <button type="button" id="delete--${i}" class="btn btn-danger">Delete</button>
+        <button type="button" id="edit--${i}" class="btn btn-info">Edit</button>
       </div>
     </div>
     `;
   });
 
-  renderToDom("#piesContainer", domString);
+  renderToDom('#piesContainer', domString);
 };
 
 // Handles the button events
 const buttonEvents = () => {
   document
-    .querySelector("#buttonContainer")
-    .addEventListener("click", handleButtonClick);
+    .querySelector('#buttonContainer')
+    .addEventListener('click', handleButtonClick);
 
-  document.querySelector("#piesContainer").addEventListener("click", deletePie);
+  document.querySelector('#piesContainer').addEventListener('click', modifyPie);
 };
 
 const init = () => {
